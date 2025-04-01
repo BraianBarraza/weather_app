8d0e6e0339a13a22ee2ec91412bbc24fbca18166
Braian <braian_019@hotmail.com>
2025-04-01 15:41:22 +0200

    Weather app creation, app, form, styles and bg image are defined

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import Form from "./components/Form.vue";
+
+
+</script>
+
+<template>
+  <h1 class="title">Weather Searcher</h1>
+
+  <div class="container weather-searcher">
+    <Form/>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+<script setup>
+const countries = [
+  {code: 'US', name: 'USA'},
+  {code: 'MX', name: 'Mexico'},
+  {code: 'AR', name: 'Argentina'},
+  {code: 'CO', name: 'Colombia'},
+  {code: 'CR', name: 'Costa Rica'},
+  {code: 'ES', name: 'Spain'},
+  {code: 'PE', name: 'Peru'}
+]
+</script>
+
+<template>
+  <form action="" class="form">
+    <div class="field">
+      <label for="city">City</label>
+      <input type="text"
+             id="city"
+             placeholder="City"
+      >
+    </div>
+    <div class="field">
+      <label for="country">Country</label>
+      <select
+      id="country"
+      >
+        <option value="">-- Select a Country--</option>
+        <option v-for="country in countries" :value="country.code">
+          {{ country.name }}
+        </option>
+      </select>
+    </div>
+
+    <input type="submit" value="Check Weather" />
+  </form>
+</template>
+
+<style scoped>
+
+</style>
